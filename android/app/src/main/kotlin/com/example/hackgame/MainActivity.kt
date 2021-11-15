cc114c4904cd8912b1c7923ff58cf5149aec24d2
Bright <bright@RUTHs-MacBook-Air.local>
2021-11-15 11:55:38 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/hackgame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hackgame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static  Color appGreen = Color.fromARGB(86, 212, 55, 1);
+  static Color appRed = Color.fromARGB(241, 41, 41, 1);
+  static Color appOrange = Color.fromARGB(253, 141, 11, 1);
+  static Color appBlue = Color.fromARGB(50, 107, 255, 1);
+  static Color appGold = Color.fromARGB(212, 175, 55, 1);
+}
